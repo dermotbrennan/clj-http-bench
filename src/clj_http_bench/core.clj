a97dd78364ec9cda9273c293ce27e6925e771732
Dermot Brennan <dermot.brennan@gmail.com>
2011-12-04 11:31:03 +0000

    Initial leiningen project

---
+++ src/clj_http_bench/core.clj
@@ -0,0 +1,1 @@
+(ns clj-http-bench.core)
---
+++ test/clj_http_bench/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-http-bench.test.core
+  (:use [clj-http-bench.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
